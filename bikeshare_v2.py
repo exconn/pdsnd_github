599477db036e5413039a39eb425da243a93800c4
Eugene Conn <exconn713@yahoo.com>
2019-11-25 11:48:21 -0600

    Added comment about GitHub repo available

--- bikeshare_v2.py
+++ bikeshare_v2.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+# There is a copy of the repo on GitHub now.
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
